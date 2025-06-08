53e0a40b2f82585cc2c2600664e9a2a24aba590d
RahatYT9 <farhanahmad.61745@gmail.com>
2025-06-08 12:11:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function redirectToDownload() {
+    const password = document.getElementById("password").value;
+    const correctPassword = "66";
 
+    if (password === correctPassword) {
+        window.location.href = "download.html";
+    } else {
+        alert("Incorrect password! Please try again.");
+    }
+}
